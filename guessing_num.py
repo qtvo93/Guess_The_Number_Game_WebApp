2508ad7c0b128dcbc0798600e0b6b69dac774e66
Quoc Thinh Vo <72519491+qtvo93@users.noreply.github.com>
2021-05-03 12:33:10 -0400

    Update guessing_num.py

--- guessing_num.py
+++ guessing_num.py
@@ -10,7 +10,7 @@ import streamlit as st
 import random
 import functools
 import time
-
+import pandas as pd
 
 st.title('Guessing the random number')
 
@@ -24,7 +24,7 @@ expander_bar.markdown("""
 
 
 #---------------------------------#
-####
+#### prevent cahce refreshing from button click
 def cache_on_button_press(label, **cache_kwargs):
    
     internal_cache_kwargs = dict(cache_kwargs)
@@ -53,8 +53,13 @@ def cache_on_button_press(label, **cache_kwargs):
             return cache_entry.return_value
         return wrapped_func
     return function_decorator
-####  
+#### 
 
+#%% Side bar
+st.sidebar.header("Please choose an option")
+menu = ["Play Game", "View Data"]
+choice = st.sidebar.selectbox("Menu", menu)    	
+#%% function declarations
 
 @cache_on_button_press("Guess")
 def authenticate(rand,gue):
@@ -63,61 +68,92 @@ def authenticate(rand,gue):
 @cache_on_button_press("Let's play")
 def play(min_num,max_num):
     return random.randint(min_num,max_num)
+ 
+#%% Section 1: header and number interval picking
+if choice == "Play Game":
+    st.header('Please choose a number range:')
+    st.text("  ")
+    min_num=st.slider("Minimum:",0,9999,1000)
+    max_num=st.slider("Maximum:",0,9999,3000)
+    
+    if min_num > max_num:
+        st.error("Max number cannot be smaller than Min number")
+        st.text("Please adjust the interval and try again")
+        st.stop().Exception()
+    min_num= int(min_num)
+    max_num= int(max_num)
+    rand_num = play(min_num,max_num)
+    
+    #%% Section 2: Generating random number and guessing
+    
+    st.write("The generated random number is between: ",min_num,"and",max_num)
+    st.header('Please enter your guess here:')
+    guess= st.text_input("Guess number:")
+    
+    #%% Authenticate guess vs random num
+          
+    if authenticate(str(rand_num), (guess)):
+        if guess == "":
+            st.text("Please enter a number before pressing Guess button!")  
+        st.write("Random number is: ",rand_num)
+        st.success('Congratulations, your guess is right!')
+        #st.write("You win the game with",t1, "guessing plays")
+              
+    else:   
+        if guess != "":        
+            if(rand_num) < int(guess):
+                st.warning('Random number is smaller than your guess')            
+            elif (rand_num) > int(guess):
+                st.error('Random number is greater than your guess')    
+        else:
+            st.text("Please enter a valid number")
+        st.stop().Exception()
+    
+    #%% Footer + Rating the app
+           
+    st.header('Did you win or have fun playing?')
+    answer = st.slider("Rate this app",0,10,5)
+    
+    if st.button("Rate"):
+        link = """ <a href="http://quoctvo.com">Thank you! Click here to go back to my website</a> """
+        st.markdown(link, unsafe_allow_html=True)
+        st.balloons()
+    
+#%% choice == View data
+import matplotlib.pyplot as plt
+
+if choice == "View Data":
+    st.text("  ")
+    player = 59
+    winner = 55
+    number_of_players = st.write("Players:",player)
+    player_win= st.write("Winners:",winner)
+    
    
-st.header('Please choose your number interval:')
-min_num=st.slider("From:",0,10,9999)
-max_num=st.slider("To:",0,10,9999)
-
-if min_num > max_num:
-    st.text("First number cannot be smaller than second number! Please choose again")
-    st.stop().Exception()
-min_num= int(min_num)
-max_num= int(max_num)
-rand_num = play(min_num,max_num)
-
-st.write("The generated random number is between: ",min_num,"and",max_num)
-st.header('Please enter your guess here:')
-guess= st.text_input("Guess number:")
-
-
-
-   
-
-
-#Authenticate guess
-      
-if authenticate(str(rand_num), (guess)):
-    if guess == "":
-        st.text("Please enter a number before pressing Guess button!")  
-    st.write("Random number is: ",rand_num)
-    st.success('Congratulations, your guess is right!')
-    #st.write("You win the game with",t1, "guessing plays")
-   
-       
-else:   
-    if guess != "":        
-        if(rand_num) < int(guess):
-            st.warning('Random number is smaller than your guess')            
-        elif (rand_num) > int(guess):
-            st.error('Random number is greater than your guess')    
-    else:
-        st.text("Please enter a valid number")
-    st.stop().Exception()
-        
-st.header('Did you win or have fun playing?')
-answer = st.slider("Rate this app",0,10,5)
-
-if st.button("Rate"):
-    link = """ <a href="http://quoctvo.com">Thank you! Click here to go back to my website</a> """
-    st.markdown(link, unsafe_allow_html=True)
-    st.balloons()
-
-
-
-
-
-
-
+    y_data=[]
+    for y in range(int(winner)):
+        y_data.append(random.randint(1,12))
+    x_data=[]
+    for x in range(int(winner)):
+        x_data.append(x+1)
+    average = int(sum(y_data) / len(y_data) )
+    best= min(y_data)
+    best_record= st.write("Best record: Player ID 34 won with ", best, "guess(es)")
+    print_average = st.write("Average guess per play:",average)
+    st.text("  ")
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    
+    ax.plot(
+        x_data,
+            y_data,
+        )
+    
+    ax.set_ylabel("Number of guess per Winner")
+    ax.set_xlabel("Number of Winner")
+    
+    st.write(fig)
+    st.write("Live time scraping data is being worked on and will be updated soon.")
 
 
 
